0e4c9d9be9a3594f0b0ccd2be462146815943b60
vitalh <VHUSHCHAA@MGIUSDSFA2.MOVADOGROUP.com>
2017-09-29 14:03:57 -0400

    Convert Eclipse to maven project #3

--- sample/runScript.bat
+++ sample/runScript.bat
@@ -1,8 +1,10 @@
 set JAVA_HOME=D:\Dropbox\work\tools\jdk8
 set path=%JAVA_HOME%\bin;%path%
 
+if %PROCESSOR_ARCHITECTURE% EQU "x86" (DLL_PATH=./x86) ELSE ( set DLL_PATH=./x64) 
+
 cd "%~dp0"
 
-java -cp "classes;lib/*" groovy.ui.GroovyMain SampleScript.groovy
+java -cp "classes;lib/*" -Djava.library.path="%DLL_PATH%" groovy.ui.GroovyMain SampleScript.groovy
 
 pause
